1c7a26b70fa2d285eafdca441259ec88d3adabdf
jbob9 <berlenskyoliver99@gmail.com>
2023-09-22 17:04:19 -0400

    begin to add webhooks for moncash and stripe

---
+++ pages/videos/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/layouts'
+
+const Videos = () => {
+  return (
+    <Layout>
+      <div className='pb-20 pt-24'>
+        
+      </div>
+    </Layout>
+  )
+}
+
+export default Videos
