157c9e257a484e66f50a0ef085fc766384a35577
jbob9 <berlenskyoliver99@gmail.com>
2023-09-30 20:25:12 -0400

    add the poppins font family

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,21 +1,23 @@
 import { ReactNode } from 'react'
 import Navbar from './navbar'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import Footer from './footer'
 import { Button } from '@/components/ui/button'
 import { VideoIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
 
 const Layout = ({ children, showVideoButton = false }: { children: ReactNode, showVideoButton?: boolean }) => {
   return (
-    <div className={`relative flex min-h-screen flex-col ${inter.className}`}>
+    <div className={`relative flex min-h-screen flex-col ${poppins.className}`}>
       <Navbar/>
       <main className="flex-1 relative">
         {children}
         {showVideoButton && 
-        <Button className='bg-main rounded-full fixed bottom-10 md:bottom-16 p-4 h-auto right-4 z-40' asChild>
+        <Button 
+        className='bg-main rounded-full fixed bottom-10 md:bottom-16 p-4 h-auto right-4 z-40 animate-bounce' 
+        asChild>
           <Link href={'/videos'}>
             <VideoIcon width={24} height={24}/>
           </Link>
